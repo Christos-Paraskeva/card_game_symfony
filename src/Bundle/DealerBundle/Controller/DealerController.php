7eab97652fac1e60ba79ce796c342737c5626ace
Christos Paraskeva <chris.paraskeva10@gmail.com>
2017-09-21 16:52:12 +0100

    Deals out cards correctly to all the players

--- src/Bundle/DealerBundle/Controller/DealerController.php
+++ src/Bundle/DealerBundle/Controller/DealerController.php
@@ -17,18 +17,11 @@ class DealerController extends Controller
     {
         $dealerService = $this->container->get('app.dealer');
         $deckOfCards = $this->get('session')->get('deckOfCards');
+        $currentPlayers = $this->get('session')->get('currentPlayers');
 
+        $dealerService->dealTheCards(7, $currentPlayers, $deckOfCards);
 
-//        $shuffleService = $this->container->get('app.shuffle');
-//        $session =  $this->container->get('session');
-//
-//        $shuffledDeck = $shuffleService->defaultShuffle($session->get('deckOfCards'));
-//        $session->set('deckOfCards', $shuffledDeck);
-//        $session->save();
-//
-//        return $this->redirect($this->generateUrl('shuffled_deck'));
-
-
+        return $this->redirect($this->generateUrl('player'));
     }
 
 }
--- src/Bundle/DealerBundle/Service/Dealer.php
+++ src/Bundle/DealerBundle/Service/Dealer.php
@@ -35,38 +35,3 @@ class Dealer {
     }
 
 }
-
-
-//"strict mode";
-//
-//function Dealer(shuffle = new Shuffle()) {
-//    this.currentPlayers = [];
-//    this.shuffle = shuffle;
-//}
-//
-//Dealer.prototype = {
-//    shuffleTheDeck: function(deck) {
-//        this.shuffle.defaultShuffle(deck);
-//    },
-//
-//    dealTheCards: function(howManyCards, currentPlayers, deck) {
-//        this.currentPlayers = currentPlayers;
-//
-//        if (deck.length - (currentPlayers.length * howManyCards) >= 0 && currentPlayers.length !== 0) {
-//            for(var i=0; i < howManyCards; i++) {
-//                for(var p=0; p < this.currentPlayers.length; p++) {
-//                    this.currentPlayers[p].cardsHeld.push(deck.splice(0, 1));
-//                }
-//      }
-//    }
-//        else {
-//            throw new Error("Cannot deal: not enough cards or players");
-//        }
-//    },
-//};
-//
-//class Dealer {
-//
-//    public function __construct () {
-//        $this->currentPlayers = [];
-//    }
